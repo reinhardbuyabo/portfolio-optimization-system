66d904d89bb7b69f437a850b8dc43256b0dda090
reinhardbuyabo <buyabor@gmail.com>
2025-09-14 23:21:29 +0300

    feat: Scaffolded Next App
    Created The Frontend Baseline of the Project
    
    Issue #2

---
+++ lib/constants/index.ts
@@ -0,0 +1,3 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "Porfolio Optimization System"
+export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESCRIPTION || "A web application for portfolio optimization and management."
+export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000"
