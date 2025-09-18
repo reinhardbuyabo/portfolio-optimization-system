81b3b9dc45e67ceb3a49e64da9fb4fdd94bee01a
reinhardbuyabo <buyabor@gmail.com>
2025-09-18 08:41:25 +0300

    feat: User Button & Sign Out
    - [x] Added UI components to enable users to sign out
    - [ ] Need to ensure that Home Page reflects current user's information
    
    Issue #2

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,37 +1,20 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "@/assets/styles/globals.css";
-import { APP_DESCRIPTION, APP_NAME, SERVER_URL } from "@/lib/constants";
-import { ThemeProvider } from "next-themes";
+import Image from "next/image";
+import loader from "@/assets/loader.gif";
 
-const inter = Inter({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-    title: {
-        template: `%s | Prostore`,
-        default: APP_NAME,
-    },
-    description: APP_DESCRIPTION,
-    metadataBase: new URL(SERVER_URL),
+const LoadingPage = () => {
+    return (
+        <div
+            style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                height: "100vh",
+                width: "100vw",
+            }}
+        >
+            <Image src={loader} height={150} width={150} alt="Loading..." />
+        </div>
+    );
 };
 
-export default function RootLayout({
-    children,
-}: Readonly<{
-    children: React.ReactNode;
-}>) {
-    return (
-        <html lang="en" suppressHydrationWarning>
-            <body className={`${inter.className} antialiased`}>
-                <ThemeProvider
-                    attribute="class"
-                    defaultTheme="light"
-                    enableSystem
-                    disableTransitionOnChange
-                >
-                    {children}
-                </ThemeProvider>
-            </body>
-        </html>
-    );
-}
+export default LoadingPage;
